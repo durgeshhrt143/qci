89fd25defbbb60bd71862bc39d5bd68578566279
Durgesh Singh <singh.durgesh5@gmail.com>
2019-09-25 17:10:15 +0530

    build

---
+++ build/js/custom.js
@@ -0,0 +1,12 @@
+$(function() {
+  $(window).scroll(function() {
+    let scroll = $(this).scrollTop();
+    if (scroll >= 15) {
+      $("body").addClass("stickyHeader");
+    } else {
+      $("body").removeClass("stickyHeader");
+    }
+  });
+
+  $("select").formSelect();
+});
---
+++ build/precache-manifest.3395786cac4fe87293672f2584eca769.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c9c9054e9182a0f7531fc056006f99a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8400976df481e35dc9b",
+    "url": "/static/css/2.182db967.chunk.css"
+  },
+  {
+    "revision": "f81b6758254ceec10b52",
+    "url": "/static/css/main.b23815d4.chunk.css"
+  },
+  {
+    "revision": "c8400976df481e35dc9b",
+    "url": "/static/js/2.2f655630.chunk.js"
+  },
+  {
+    "revision": "f81b6758254ceec10b52",
+    "url": "/static/js/main.125b83b5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "df29add3e2abf9f718136dd17b3ec0ca",
+    "url": "/static/media/404.df29add3.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f8e9b1762ccb9cfc7769795cc7ce4e3c",
+    "url": "/static/media/o.f8e9b176.png"
+  },
+  {
+    "revision": "8849a3cd1944d8d4fb0d9fa54e272a55",
+    "url": "/static/media/qci-logo.8849a3cd.jpg"
+  },
+  {
+    "revision": "42fe219d640e5f6491d62db54dfaeaca",
+    "url": "/static/media/verify.42fe219d.gif"
+  }
+]);
